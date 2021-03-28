a2522e0c1078e16b195e05fa803bd7f2210d8679
Imam Hossain Roni <imamhossainroni95@gmail.com>
2021-03-29 00:09:03 +0600

    django version changed from 2 to 3

--- myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render_to_response,render
-from django.http import HttpResponse
-from .models import  Language
-from utils.db import session
-
-def index(request,**kwargs):
-    lang = session.query(Language).all()
-    data = {'lang': lang}
-    return render(request,'index.html',data)
